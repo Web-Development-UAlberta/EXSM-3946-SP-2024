4633335d8a88a31a48392fccb97d9995e4da1295
James Grieve <jamesonrgrieve@gmail.com>
2024-07-08 20:31:24 -0600

    Implement edit dialog;

--- FrontEnd/src/app/page.jsx
+++ FrontEnd/src/app/page.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import useSWR from 'swr';
 import axios from 'axios';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import EditDialog from 'jrgcomponents/EditDialog';
 export default function Home() {
   const { data } = useSWR('/weather', async () => {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URI}/weatherforecast`);
@@ -17,6 +18,15 @@ export default function Home() {
           <Typography variant='body1'>{forecast.temperatureC}°C</Typography>
           <Typography variant='body1'>{forecast.temperatureF}°F</Typography>
           <Typography variant='body1'>{forecast.summary}</Typography>
+          <EditDialog
+            toUpdate={forecast}
+            onConfirm={async (updated) => {
+              console.log(updated);
+              //await axios.put(`${process.env.NEXT_PUBLIC_API_URI}/weatherforecast`, updated);
+            }}
+            ButtonComponent={Button}
+            ButtonProps={{ children: 'Update', color: 'primary', variant: 'contained' }}
+          />
         </Box>
       ))}
     </Box>
