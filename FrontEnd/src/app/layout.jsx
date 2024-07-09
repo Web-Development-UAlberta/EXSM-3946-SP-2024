8a5563e2cfad4514fc06a50c1fc426645dce62c8
James Grieve <jamesonrgrieve@gmail.com>
2024-07-08 20:53:26 -0600

    Themed app wrapper;

--- FrontEnd/src/app/layout.jsx
+++ FrontEnd/src/app/layout.jsx
@@ -1,32 +1,37 @@
+'use client';
 import 'jrgcomponents/Style/Global';
-import AppWrapper from 'jrgcomponents/AppWrapper/Wrapper';
+import ThemedAppWrapper from 'jrgcomponents/AppWrapper/Wrapper/Themed';
 import React from 'react';
 
+/*
 export const metadata = {
   title: 'UAlberta FullStack',
   description: 'An application created by a student of the University of Alberta Fullstack Web Application Development program.',
 };
+*/
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>
-        <AppWrapper
-          keepThemeToggles
-          header={{
-            components: {
-              center: 'UAlberta FullStack',
-              right: 'test',
+        <ThemedAppWrapper
+          appWrapperConfig={{
+            keepThemeToggles: true,
+            header: {
+              components: {
+                center: 'UAlberta FullStack',
+                right: 'test',
+              },
             },
-          }}
-          footer={{
-            components: {
-              center: 'Test',
+            footer: {
+              components: {
+                center: 'Test',
+              },
             },
           }}
         >
           {children}
-        </AppWrapper>
+        </ThemedAppWrapper>
       </body>
     </html>
   );
