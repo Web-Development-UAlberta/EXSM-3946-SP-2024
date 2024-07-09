80b2b8382551340840de0ecd41d5a304b119a268
James Grieve <jamesonrgrieve@gmail.com>
2024-07-08 19:12:46 -0600

    Working front end and back end;

---
+++ BackEnd/src/DotNetAPIDemo.Tests/BackEnd/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DotNetAPIDemo.Tests.BackEnd;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
