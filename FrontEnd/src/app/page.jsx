79ba74004bb7efc288af33e43d0a718142003dbe
James Grieve <jamesonrgrieve@gmail.com>
2024-07-08 20:14:24 -0600

    Output data from back end in NextJS;

--- FrontEnd/src/app/page.jsx
+++ FrontEnd/src/app/page.jsx
@@ -6,12 +6,19 @@ import { Box, Typography } from '@mui/material';
 export default function Home() {
   const { data } = useSWR('/weather', async () => {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URI}/weatherforecast`);
-    return response.json();
+    return response.data;
   });
-  console.log(data);
   return (
     <Box component='main'>
       <Typography variant='h1'>Hello, World!</Typography>
+      {data?.map((forecast) => (
+        <Box key={forecast.date}>
+          <Typography variant='h2'>{forecast.date}</Typography>
+          <Typography variant='body1'>{forecast.temperatureC}°C</Typography>
+          <Typography variant='body1'>{forecast.temperatureF}°F</Typography>
+          <Typography variant='body1'>{forecast.summary}</Typography>
+        </Box>
+      ))}
     </Box>
   );
 }
