ad997a56e0ec68bac11d9f1d6770f61769bcd246
James Grieve <jamesonrgrieve@gmail.com>
2024-07-19 20:49:50 -0600

    Routing is functional; Joke output;

--- FrontEnd/src/app/page.jsx
+++ FrontEnd/src/app/page.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import useSWR, { mutate } from 'swr';
 import axios from 'axios';
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
 import { Comment, Edit } from '@mui/icons-material';
 import { DataGrid } from '@mui/x-data-grid';
 import EditDialog from 'jrgcomponents/EditDialog';
@@ -10,54 +10,20 @@ import Dialog from 'jrgcomponents/Dialog';
 import Comments from './Comments';
 
 export default function Home() {
-  const { data: rows } = useSWR(
-    '/posts',
+  const { data } = useSWR(
+    '/joke',
     async () => {
-      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URI}/api/Post`);
+      const response = await axios.get('https://api.chucknorris.io/jokes/random?category=food');
       return response.data;
     },
     {
       fallbackData: [],
     },
   );
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    { field: 'title', headerName: 'Title', width: 150 },
-    { field: 'content', headerName: 'Content', width: 150 },
-    { field: 'createdAt', headerName: 'CreatedAt', width: 150 },
-    {
-      field: 'actions',
-      headerName: 'Actions',
-      width: 150,
-      renderCell: (cellValues) => {
-        return (
-          <>
-            <Dialog
-              title={`Comments of Post #${cellValues.row.id}`}
-              content={<Comments postID={cellValues.row.id} />}
-              ButtonComponent={IconButton}
-              ButtonProps={{ children: <Comment /> }}
-            />
 
-            <EditDialog
-              toUpdate={cellValues.row}
-              excludeFields={['id', 'createdAt']}
-              ButtonComponent={IconButton}
-              ButtonProps={{ children: <Edit /> }}
-              onConfirm={async (data) => {
-                console.log('New Data:', data);
-                await axios.put(`${process.env.NEXT_PUBLIC_API_URI}/api/Post/${cellValues.row.id}`, { ...cellValues.row, ...data });
-                mutate('/posts');
-              }}
-            />
-          </>
-        );
-      },
-    },
-  ];
   return (
     <Box component='main'>
-      <DataGrid rows={rows} columns={columns} />
+      <Typography variant='body1'>{data.value}</Typography>
     </Box>
   );
 }
